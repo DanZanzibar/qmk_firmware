dd69dec4d879db683a235a2920cbf392e29b3ea5
Zan Owsley <zanowsley@gmail.com>
2024-11-07 10:14:30 -0700

    Added DanZanzibar keyboard

--- keyboards/lily58/keymaps/DanZanzibar/config.h
+++ keyboards/lily58/keymaps/DanZanzibar/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define QUICK_TAP_TERM 0
-#define TAPPING_TERM 100
+#define TAPPING_TERM 500
 
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
@@ -39,6 +39,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
 
 // Underglow
 /*
